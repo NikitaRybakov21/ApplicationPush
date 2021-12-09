11277ce2a28923f37e84bcd043961ffa1f27ca31
NikitaRybakov21 <nikitarybakov044@gmail.com>
2021-12-09 15:39:51 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplicationpush/MessagingService.kt
@@ -0,0 +1,19 @@
+package com.example.myapplicationpush
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import android.util.Log
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+
+
+class MessagingService : FirebaseMessagingService() {
+
+}
