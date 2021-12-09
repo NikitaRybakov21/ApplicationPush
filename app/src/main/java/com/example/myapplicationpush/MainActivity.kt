3dcbf5d977aa09def6638a3b70759ac2c207747d
NikitaRybakov21 <nikitarybakov044@gmail.com>
2021-12-09 15:40:43 +0300

    push_DZ

--- app/src/main/java/com/example/myapplicationpush/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationpush/MainActivity.kt
@@ -4,13 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 
-
+private const val ticketIdKey = "ticketId"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        val ticketId = intent.extras?.getString(ticketIdKey, "no value") ?: "no value"
+        Toast.makeText(applicationContext, ticketId, Toast.LENGTH_SHORT).show()
     }
 }
